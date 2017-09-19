823c74f45b4478120837cdbecc25008a6bdafa27
Aaron Montoya <aatoya7@gmail.com>
2017-09-19 12:40:45 -0600

    Added a filter for inputs using if/else if/else statements

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -29,8 +29,41 @@ public class MonsterController
 		System.out.println(currentMonster.getName() + " wants to know how many tentacles you want to eat, please type in how many.");
 		double consumed_tentacles = myScanner.nextDouble();
 		currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - consumed_tentacles);
-		System.out.println(currentMonster);		
+		System.out.println(currentMonster);
+		
+		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
+		int arm_eat = myScanner.nextInt();
+		
+		if(arm_eat == 0)
+		{
+			System.out.println("Not hungry? So sad...");
+		}
+		else if(arm_eat < 0) {
+			System.out.println("Reality is hard for you...It is impossible to eat a negative amount.");
+		}
+		else if(currentMonster.getArmCount() - arm_eat < 0)
+		{
+			System.out.println("Math is hard for you...You can't eat more arms then I have.");
+		}
+		else
+		{
+			currentMonster.setArmCount(currentMonster.getArmCount() - arm_eat);
+			System.out.println("Okay, now I have " + currentMonster.getArmCount() + " arm(s).");
+		}
 		
 		myScanner.close();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
